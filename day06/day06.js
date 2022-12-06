ead3e61584f4768fcd74a9a86dd8ebc21957fe62
Marcos Cavalcante <marcos.cavalcante@test-and-trace.nhs.uk>
2022-12-06 05:16:04 +0000

    feat: add day06 solution - datastream problem

---
+++ day06/day06.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const [ datastream ] = fs
+    .readFileSync('input.txt', 'utf8')
+    .split("\n")
+    .map(row => row);
+
+
+const DISTINCT_CHARACTERS = 14
+const isOutOfBounds = (strLength, index) = index > strLength;
+
+for(let i=0; i < datastream.length; i++) {
+    
+    if (isOutOfBounds(datastream.length, i+DISTINCT_CHARACTERS)) continue;
+    
+    const marker = datastream.slice(i, i+DISTINCT_CHARACTERS);
+    const markerSet = new Set(marker.split(""));
+
+    if (markerSet.size === DISTINCT_CHARACTERS) {
+        console.log("Final Marker is: ", i+DISTINCT_CHARACTERS);
+        break;
+    }
+}
